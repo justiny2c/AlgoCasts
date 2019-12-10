56acb06f592aeb0f06ab478ee7bfdcf52dd809ee
justiny2c <justiny2c@gmail.com>
2019-12-10 13:00:47 -0800

    Added insertLast function

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -53,6 +53,42 @@ class LinkedList {
   clear() {
     this.head = null;
   }
+
+  removeFirst() {
+    if (!this.head) {
+      return;
+    }
+    this.head = this.head.next;
+  }
+
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+
+    let prev = this.head;
+    let node = this.head.next;
+    while (node.next) {
+      prev = node;
+      node = node.next;
+    }
+    prev.next = null;
+  }
+
+  insertLast(data) {
+    const last = this.getLast();
+    if (last) {
+      // There are existing nodes
+      last.next = new Node(data);
+    } else {
+      // Chain is empty
+      this.head = new Node(data);
+    }
+  }
 }
 
 // const list = new LinkedList();
