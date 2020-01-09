13176ce766966c94ecadf5d7afb4edd6e2a8b650
justiny2c <justiny2c@gmail.com>
2020-01-09 13:25:29 -0800

    Levelwidth complete. Added more comments

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -14,7 +14,22 @@
 function levelWidth(root) {
   // root is a node we are working with
   // typically with "width", we are thinking about a breadth first traversal
-  // return width of each level
+  let arr = [root, 's'];
+  let counters = [0];
+
+  while (arr.length > 1) {
+    const node = arr.shift(); // remove first element in arr
+
+    if (node === 's') {
+      counters.push(0);
+      arr.push('s'); // push "s" back into end of arr
+    } else {
+      arr.push(...node.children); // push all of node's children
+      counters[counters.length - 1]++; // find last element in counters and increment by 1
+    }
+  }
+  // return width of each level in an array
+  return counters;
 }
 
 module.exports = levelWidth;
