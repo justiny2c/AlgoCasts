e873a5a5d5b5e76926eb1eb18afec05bd095ead1
justiny2c <justiny2c@gmail.com>
2020-01-27 16:34:13 -0800

    Sorting. bubbleSort complete

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,19 +2,25 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+  // n^2 runtime
+  // Implement bubblesort
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let lesser = arr[j + 1];
+        arr[j + 1] = arr[j]; // swap greater value with lesser
+        arr[j] = lesser;
+      }
+    }
+  }
+  // return sorted array
+  return arr;
 }
 
-function selectionSort(arr) {
+function selectionSort(arr) {}
 
-}
+function mergeSort(arr) {}
 
-function mergeSort(arr) {
-
-}
-
-function merge(left, right) {
-
-}
+function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
